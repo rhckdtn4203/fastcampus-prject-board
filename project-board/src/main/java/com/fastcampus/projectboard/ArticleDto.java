a2e6c846e58beecb4aaec3348c776c19806c9b20
changsu.go <mrk100409@gmail.com>
2023-02-20 16:48:29 +0900

    ArticleDto 작업분

---
+++ project-board/src/main/java/com/fastcampus/projectboard/ArticleDto.java
@@ -0,0 +1,15 @@
+package com.fastcampus.projectboard;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record ArticleDto(
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+        ) implements Serializable {
+}
